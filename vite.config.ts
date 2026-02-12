2283a09e8c8a326459bb10cb4b74346ff7124387
zrncrblln <zorencorbillon@gmail.com>
2026-02-12 14:01:36 +0800

    Deploy to GitHub Pages with .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: "/marci_metzger/",
+  build: {
+    outDir: "docs",
+  },
   server: {
     host: "::",
     port: 8080,
