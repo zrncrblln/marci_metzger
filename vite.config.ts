ca56c1097c24c46977bd50215a592fe6b5da9953
zrncrblln <zorencorbillon@gmail.com>
2026-02-12 16:07:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/",
+  base: "/marci_metzger/",
   build: {
-    outDir: "docs",
+    outDir: "root",
   },
   server: {
     host: "::",
